8e55826869f4c545d95eb29611646bfdda083a23
Mas G <35314624+Loominagit@users.noreply.github.com>
2021-08-22 23:24:29 +0700

    Updated ping.js because I forgot to add MessageEmbed reference

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,12 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
+    
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Checks bot\'s latency.'),
+
 	async execute(interaction) {
 		const client = interaction.client;
         return interaction.reply({ content: 'Pinging...', fetchReply: true }).then(reply => {
