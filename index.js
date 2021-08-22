e6a0aca6063dfba0aa7cf33947cae4b33ee24824
Mas G <35314624+Loominagit@users.noreply.github.com>
2021-08-22 22:45:09 +0700

    initial commit

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client(
+    {intents: [
+        Intents.FLAGS.GUILDS
+    ]}
+);
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
