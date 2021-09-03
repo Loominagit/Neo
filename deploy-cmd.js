09cd11aba88c5164eb31c509a94aac6357b5ddbe
Mas G <anakmasgadinggondokusumo@gmail.com>
2021-09-03 10:48:33 +0700

    Replacing `print` to `console.log`

--- deploy-cmd.js
+++ deploy-cmd.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
+require('./custom_log.js');
 
 const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 
-const { print, warn, error } = require('./custom_log.js');
-
 const clientId = process.env.CLIENT_ID;
 const token = process.env.TOKEN;
 
-print('Preparing commands...');
+console.log('Preparing commands...');
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -18,19 +17,19 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
-print('Preparing Discord.js REST...');
+console.log('Preparing Discord.js REST...');
 const rest = new REST({ version: '9' }).setToken(token);
 
 (async () => {
-	print('Attempting to register slash (/) commands...');
+	console.log('Attempting to register slash (/) commands...');
 	try {
 		await rest.put(
 			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 
-		print('Successfully registered slash (/) commands!');
+		console.log('Successfully registered slash (/) commands!');
 	} catch (err) {
-		error(err);
+		console.error(err);
 	}
 })();
